e408136ccb97a641c97e070f4bc5c3b808a16fbd
Kevin Chen <kchen1250@gmail.com>
2023-04-13 15:30:33 -0400

    fix stuff

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 require('dotenv').config()
 const express = require('express');
+const path = require('path');
 const app = express();
 const Airtable = require('airtable');
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.post('/submit', (req, res) => {
   console.log(req.body);
   base('tblKl1yEoaQWmCXYJ').create(req.body,
-  function(err, record) {
-    // if (err) {
-    //   console.error(err);
-    //   res.status(500).send('Error occurred while sending data to Airtable.');
-    // } else {
-      // console.log(records);
+    function(err, record) {
       res.send({err: err, record: record});
-    // }
-  });
+    }
+  );
 });
 
 app.post('/get-first-page', (req, res) => {
@@ -28,7 +24,7 @@ app.post('/get-first-page', (req, res) => {
     function(err, records) {
       res.send({err: err, records: records});
     }
-  )
+  );
 });
 
 const port = 3000;
