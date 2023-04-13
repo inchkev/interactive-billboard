8727b4ae0bd31aab069af8f44f378841e5b1ac91
Kevin Chen <kchen1250@gmail.com>
2023-04-13 15:37:11 -0400

    Changes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/get-first-page', (req, res) => {
   );
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`)
 });
